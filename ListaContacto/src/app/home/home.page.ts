22eacaa99bf09390caf20f5628133f7732c267c9
Rardati <rardati601@g.educaand.es>
2024-11-27 13:34:10 +0100

    Guardar

---
+++ ListaContacto/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  users: any;
+
+  constructor(private httpClient: HttpClient) {
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=20').pipe(map((res:any) => res['results']));
+  }
+
+}
